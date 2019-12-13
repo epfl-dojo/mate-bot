dc0b56746ad4bd4b8b96be2daddf8c11e6e7de45
TacticsCH <alexandre.javet@hotmail.com>
2019-12-13 16:58:27 +0100

    [ADD] /start command

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const TeleBot = require('telebot');
-const Secrets = require('./secrets.json')
+const Secrets = require('./secrets.json');
 const bot = new TeleBot(Secrets.BOT_TOKEN);
-bot.on('text', (msg) => msg.reply.text(msg.text));
+
+bot.on(['/start'], (msg) => {
+
+  msg.reply.text("Hello! I am Club Mate Bot, I'm here to help you manage your mate consumption. Please type '/help' to begin!")
+
+});
 
 bot.start();
